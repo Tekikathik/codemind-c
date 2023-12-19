42a2584a86ce0e0bc1d97a95f378d739ac9407a2
Tekikathik <147475789+Tekikathik@users.noreply.github.com>
2023-12-19 10:40:35 +0530

    #include<stdio.h> int main (){     int a,i,j;     scanf("%d",&a);     int arr[a];     for (i=0;i<a;i++){         scanf("%d",&arr[i]);     }     for (j=a-1;j>=0;j--){         if (arr[j]%2!=0){             printf("%d",arr[j]);             break;         }
        }
    }

---
+++ Last_odd_number_in_an_Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+    int a,i,j;
+    scanf("%d",&a);
+    int arr[a];
+    for (i=0;i<a;i++){
+        scanf("%d",&arr[i]);
+    }
+    for (j=a-1;j>=0;j--){
+        if (arr[j]%2!=0){
+            printf("%d",arr[j]);
+            break;
+        }
+        
+    }
+}
