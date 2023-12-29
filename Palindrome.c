f7430bfb3cf1c0df5ec17247e0c83568a4345117
Tekikathik <147475789+Tekikathik@users.noreply.github.com>
2023-12-29 20:12:27 +0530

    #include<stdio.h> int main (){     int a,b,c=0,d;     scanf("%d",&a);     d=a;     while (a!=0){         b=a%10;         c=c*10+b;         a=a/10;     }     if (c==d){         printf("True");     }     else printf("False"); }

---
+++ Palindrome.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+    int a,b,c=0,d;
+    scanf("%d",&a);
+    d=a;
+    while (a!=0){
+        b=a%10;
+        c=c*10+b;
+        a=a/10;
+    }
+    if (c==d){
+        printf("True");
+    }
+    else printf("False");
+}
