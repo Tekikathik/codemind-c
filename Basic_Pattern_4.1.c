73ea0d88fdf6822c7d3e370504d6674f69c77f7b
Tekikathik <147475789+Tekikathik@users.noreply.github.com>
2023-12-31 13:25:52 +0530

    #include<stdio.h> int main (){     int a;     scanf("%d",&a);     for (int i=1;i<=a;i++){         for (int j=1;j<=a;j++){             if (i%2==0)             printf("%d ",1);             else printf("%d ",0);         }         printf(" ");     } }

---
+++ Basic_Pattern_4.1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+    int a;
+    scanf("%d",&a);
+    for (int i=1;i<=a;i++){
+        for (int j=1;j<=a;j++){
+            if (i%2==0)
+            printf("%d ",1);
+            else printf("%d ",0);
+        }
+        printf("
+");
+    }
+}
