2f1876241bad341d33a446de1219fe986b55c601
teki karthik <147475789+Tekikathik@users.noreply.github.com>
2024-03-08 23:02:34 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     printf("%d",n*(n-1)/2); }

--- Program_to_find_the_maximum_number_of_handshakes_.c
+++ Program_to_find_the_maximum_number_of_handshakes_.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
-int main (){
-    int a,b;
-    scanf("%d",&a);
-    b=(a*(a-1))/2;
-    printf("%d",b);
-    
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d",n*(n-1)/2);
 }
